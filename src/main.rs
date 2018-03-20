8bc609710641706ae71520e633f4131fe0e7f21d
Lucas Cunha <lucas.cunha@eitv.com.br>
2018-03-20 11:31:32 -0300

    Struct Mutex working

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ extern crate serde;
 #[macro_use] extern crate serde_derive;
 #[macro_use] extern crate rouille;
 
+use std::sync::{Arc, Mutex};
+use std::fs::File;
+
 #[derive(Serialize)]
 struct Item {
     id: usize,
@@ -10,19 +13,32 @@ struct Item {
 
 #[derive(Serialize)]
 struct Main {
-    mut items: Vec<Item>
+    items: Vec<Item>
 }
 
 fn main() {
     println!("Now listening on localhost:8000");
 
-    let main = Main { items: Vec::new() };
+    let main = Arc::new(Mutex::new(Main { items: Vec::new() }));
 
     rouille::start_server("localhost:8000", move |request| {
         router!(request,
             (GET) (/) => {
-                main.items.push(Item { id: main.items.len(), value: "test".to_owned() });
-                rouille::Response::json(&main)
+                let file = File::open("tasko-web/index.html").unwrap();
+                rouille::Response::from_file("text/html", file)
+            },
+
+            (GET) (/json) => {
+                let data = main.lock().unwrap();
+                rouille::Response::json(&*data)
+            },
+
+            (POST) (/new) => {
+                let mut data = main.lock().unwrap();
+                let size = data.items.len();
+
+                data.items.push(Item { id: size, value: "test".to_owned() });
+                rouille::Response::text("Success")
             },
 
             _ => {
