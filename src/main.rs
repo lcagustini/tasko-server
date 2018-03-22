b8a76940f6d4ed4f49c6e8d909ebf74291952332
Lucas Cunha <lucas.cunha@eitv.com.br>
2018-03-22 15:40:24 -0300

    Static file handler

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ enum Item {
 
 #[derive(Serialize)]
 struct List {
-    id: usize,
+    name: String,
     items: Vec<Item>,
 }
 
@@ -34,15 +34,70 @@ fn main() {
     let main = Arc::new(Mutex::new(Main { boards: Vec::new(), size: 0 }));
 
     rouille::start_server("localhost:8000", move |request| {
+        {
+            let response = rouille::match_assets(&request, ".");
+            if response.is_success() {
+                return response;
+            }
+        }
+
         router!(request,
                 (GET) (/) => {
                     let file = File::open("tasko-web/index.html").unwrap();
                     rouille::Response::from_file("text/html", file)
                 },
 
+                (GET) (/interface) => {
+                    let file = File::open("tasko-web/interface.html").unwrap();
+                    rouille::Response::from_file("text/html", file)
+                },
+
                 (GET) (/json) => {
                     let data = main.lock().unwrap();
                     rouille::Response::json(&*data)
+                },
+
+                (POST) (/newText) => {
+                    let mut data = main.lock().unwrap();
+
+                    let input = try_or_400!(post_input!(request, {
+                        board: String,
+                        list: String,
+                        text: String,
+                    }));
+
+                    for i in 0..data.boards.len() {
+                        if data.boards[i].name == input.board {
+                            for j in 0..data.boards[i].lists.len() {
+                                if data.boards[i].lists[j].name == input.list {
+                                    data.boards[i].lists[j].items.push(Item::Text(input.text));
+                                    data.size += 1;
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    }
+
+                    rouille::Response::redirect_303("/")
+                },
+
+                (POST) (/newList) => {
+                    let mut data = main.lock().unwrap();
+
+                    let input = try_or_400!(post_input!(request, {
+                        board: String,
+                        name: String,
+                    }));
+
+                    for i in 0..data.boards.len() {
+                        if data.boards[i].name == input.board {
+                            data.boards[i].lists.push(List { name: input.name, items: Vec::new() });
+                            break;
+                        }
+                    }
+
+                    rouille::Response::redirect_303("/")
                 },
 
                 (POST) (/newBoard) => {
@@ -54,7 +109,7 @@ fn main() {
 
                     data.boards.push(Board { name: input.name, lists: Vec::new() });
 
-                    rouille::Response::text("Success")
+                    rouille::Response::redirect_303("/")
                 },
 
                 _ => {
